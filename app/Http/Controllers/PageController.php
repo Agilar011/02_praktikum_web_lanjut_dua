c6b2b716ce39c97326d5379e45993def22243ee0
Agilar011 <88954314+Agilar011@users.noreply.github.com>
2023-02-27 00:48:41 +0700

    Commit Kedua

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,13 +6,9 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function index(){
-        return 'Selamat Datang';
-    }
 
-    public function about(){
-        return 'NIM : 2141720106  <br> Nama : Agilar Gumilar';
-    }
+
+
 
     public function articles($id){
         return 'Halaman Artikel dengan Id ' .$id;
